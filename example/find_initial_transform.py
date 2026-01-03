636a6f552a87beb830881d0f3f9d8ece65e04780
ShaoxiongYao <syao16@illinois.edu>
2026-01-03 17:15:14 -0500

    support find init transform for multi-camera

--- example/find_initial_transform.py
+++ example/find_initial_transform.py
@@ -24,38 +24,43 @@ def pick_correspondences(
     vis.destroy_window()
     return vis.get_picked_points()
 
-source_pcd = o3d.io.read_point_cloud("data/test1001/pcd_camera2_000.ply")
-target_pcd = o3d.io.read_point_cloud("data/test1001/pcd_robot_000.ply")
+data_dir = "data/test0102_2"
+calib_frame_id = 5
 
-# manually select correspondences
-while True:
-    source_points_indices = pick_correspondences(source_pcd)
-    target_points_indices = pick_correspondences(target_pcd)
-    if (
-        (not len(source_points_indices) >= 3) or
-        (len(source_points_indices) != len(target_points_indices))
-    ): 
-        print(
-            "Correspondences not selected correctly. "
-            "Please select at least three correspondences. \n"
-            f"{len(source_points_indices)} source points selected, "
-            f"{len(target_points_indices)} target points selected. \n"
-            "Please try again. \n"
+for camera_index in [1, 2]:
+
+    source_pcd = o3d.io.read_point_cloud(f"{data_dir}/pcd_camera{camera_index}_{calib_frame_id:03d}.ply")
+    target_pcd = o3d.io.read_point_cloud(f"{data_dir}/pcd_robot_{calib_frame_id:03d}.ply")
+
+    # manually select correspondences
+    while True:
+        source_points_indices = pick_correspondences(source_pcd)
+        target_points_indices = pick_correspondences(target_pcd)
+        if (
+            (not len(source_points_indices) >= 3) or
+            (len(source_points_indices) != len(target_points_indices))
+        ): 
+            print(
+                "Correspondences not selected correctly. "
+                "Please select at least three correspondences. \n"
+                f"{len(source_points_indices)} source points selected, "
+                f"{len(target_points_indices)} target points selected. \n"
+                "Please try again. \n"
+            )
+        else:
+            break
+
+    # construct initial guess
+    p2p = o3d.pipelines.registration.TransformationEstimationPointToPoint()
+    init_guess = p2p.compute_transformation(
+        source_pcd,
+        target_pcd,
+        o3d.utility.Vector2iVector(
+            np.stack([
+                source_points_indices,
+                target_points_indices
+            ], axis=1)
         )
-    else:
-        break
-
-# construct initial guess
-p2p = o3d.pipelines.registration.TransformationEstimationPointToPoint()
-init_guess = p2p.compute_transformation(
-    source_pcd,
-    target_pcd,
-    o3d.utility.Vector2iVector(
-        np.stack([
-            source_points_indices,
-            target_points_indices
-        ], axis=1)
     )
-)
-np.save('initial_transform2.npy', np.array(init_guess))
-print("Initial guess: \n", init_guess)
+    np.save(f'{data_dir}/initial_transform{camera_index}.npy', np.array(init_guess))
+    print("Initial guess: \n", init_guess)
